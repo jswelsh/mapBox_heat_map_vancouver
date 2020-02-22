2c5a351dd3b7e22f42ca76c6bef31c2a0663597d
jswelsh <james.sinclair.welsh@gmail.com>
2020-02-22 10:56:17 -0800

    add layers and zoom conditions

--- script.js
+++ script.js
@@ -11,7 +11,60 @@ map.on('load', function() {
     type: 'geojson',
     data: './trees.geojson'
   });
-});
+  map.addLayer({
+    id: 'trees-heat',
+    type: 'heatmap',
+    source: 'trees',
+    maxzoom: 15,
+    paint: {
+      // increase weight as diameter breast height increases
+      'heatmap-weight': {
+        property: 'height_range_id',
+        type: 'exponential',
+        stops: [
+          [0,0],
+          [10,1]
+        ]
+      },
+      // increase intensity as zoom level increases
+      'heatmap-intensity': {
+        stops: [
+          [11, 1],
+          [15, 3]
+        ]
+      },
+      // assign color values be applied to points depending on their density
+      'heatmap-color': [
+        'interpolate',
+        ['linear'],
+        ['heatmap-density'],
+        0, 'rgba(236,222,239,0)',
+        0.2, 'rgb(208,209,230)',
+        0.4, 'rgb(166,189,219)',
+        0.6, 'rgb(103,169,207)',
+        0.8, 'rgb(28,144,153)'
+      ],
+      // increase radius as zoom increases
+      'heatmap-radius': {
+        stops: [
+          [11, 15],
+          [15, 20]
+        ]
+      },
+      // decrease opacity to transition into the circle layer
+      'heatmap-opacity': {
+        default: 1,
+        stops: [
+          [14, 1],
+          [15, 0]
+        ]
+      },
+    }
+  }, 'waterway-label');
+
+
+  });
+
 map.on('click', 'trees-point', function(e) {
   console.log(e)
   new mapboxgl.Popup()
